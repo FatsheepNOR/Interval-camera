1048b2f1fdcb8f67bed01214d2bea91b7b9daa1a
FatsheepNOR <fatsheep200@gmail.com>
2016-03-18 22:05:33 +0100

    0.3
    added timer delay

---
+++ Assets/Scripts/IntervalTimer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntervalTimer : MonoBehaviour {
+
+    public float delay = 5;
+	public bool isActive = false;
+	public WebCamPhotoCamera WBCPC;
+
+	private float currentDelay = 0;
+
+	// Use this for initialization
+	void Start () {
+		currentDelay = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isActive) {
+
+			currentDelay -= 1*Time.deltaTime;
+
+			if (currentDelay <= 0){
+				takePic();
+			}
+
+		}	
+	}
+
+	public void changeActiveState (bool state){
+		isActive = state;
+	}
+
+	public void changeDelay (string cdelay){
+		float ndelay = float.Parse (cdelay);
+		delay = ndelay;
+	}
+
+	void takePic(){
+		WBCPC.Snap();
+		currentDelay = delay;
+	}
+}
